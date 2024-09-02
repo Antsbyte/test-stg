d3323a8754a25da5d615a18d9caaf26191f735ce
Felipe Angel Regalado Zuñiga <angelfeli@Mac-mini-de-Felipe.local>
2024-09-02 16:36:46 -0600

    feat(index): first commit

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs()
+  ]
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+import { execSync } from 'child_process';
+import fs from 'fs';
+import path from 'path';
+
+try {
+    const modifiedFiles = execSync('git diff --name-only --cached', {encoding: 'utf-8'}).trim().split('\n');
+    const testFilesPath = modifiedFiles.filter(file => file.match(/\.(js|jsx|ts|tsx)$/))
+                                    .map(file => path.dirname(file) + '/test/' + path.basename(file));
+    const testFiles = testFilesPath.filter(file => fs.existsSync(file));
+    if(testFiles.length > 0) {
+        execSync(`npm jest ${testFiles.join(' ') -u}`)
+    }
+} catch (error) {
+    console.log('Error while executing tests');
+    process.exit(1);
+}
